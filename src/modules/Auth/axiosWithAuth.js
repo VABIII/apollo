cc239870f2eda429ff2d77c7c0ae513d19187d41
VABIII <plybyvern6@aol.com>
2024-03-19 12:43:57 -0400

    new stuff

---
+++ src/modules/Auth/axiosWithAuth.js
@@ -0,0 +1,60 @@
+import axios from 'axios';
+
+
+const axiosWithAuth = ()=> {
+    const token = localStorage.getItem('token');
+
+    return axios.create({
+        headers: {
+            authorization: token
+        },
+        baseURL: process.env.REACT_APP_BASE_URL,
+    });
+}
+
+export default axiosWithAuth;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
