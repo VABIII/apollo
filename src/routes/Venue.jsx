e4fbcb8d9fadcf57a43f70004bff0dfe3001dc2d
VABIII <plybyvern6@aol.com>
2025-01-04 00:15:42 -0500

    Added venue page styles and navigation logic

--- src/routes/Venue.jsx
+++ src/routes/Venue.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import {Box, Grid2, styled} from "@mui/material";
-import {ImgStyles} from "../components/styledComponents/index.jsx";
+import {Box, Card, Grid2, styled, Typography} from "@mui/material";
 import { useNavigate } from 'react-router-dom'
+import {colors} from "../theme.js";
+import {NoShowTitleStyles, SubtitleStyles} from "../components/styledComponents/index.jsx";
 
 
 const VenueContainerStyles = styled(Box)(({theme}) => ({
@@ -50,6 +51,55 @@ const VenueStyles = styled(Grid2)(({theme}) => ({
     }
 }));
 
+const VenueCard = styled(Card)(({theme}) => ({
+    // width: "100%",
+    display: "flex",
+    width:"45%",
+    // padding: theme.spacing(3),
+    borderRadius: theme.shape.borderRadius,
+    margin: theme.spacing(4),
+    boxShadow: theme.shadows[5],
+    [theme.breakpoints.down("sm")]: {
+        width: "100%",
+        justifyContent: "flex-start",
+    }
+}))
+
+const ImgStyles = styled(Box)(({theme}) => ({
+    maxWidth: '33%',
+
+    borderRadius: 10,
+    margin: theme.spacing(2),
+    [theme.breakpoints.down("sm")]: {
+        maxWidth: '40%',
+    }
+
+}));
+
+const TextStyles = styled(Typography)(({theme}) => ({
+    // color: colors.get("nightMoon"),
+    color: colors.get("darkGrey2"),
+    fontSize: '1.5rem',
+    padding: theme.spacing(4),
+    [theme.breakpoints.down("sm")]: {
+        paddingBottom: theme.spacing(2),
+    }
+}));
+
+const VenuesTitleStyles = styled(Typography)(({theme}) => ({
+    color: colors.get("nightMoon"),
+    fontWeight: 500,
+    paddingLeft: theme.spacing(4),
+    paddingRight: theme.spacing(4),
+    // paddingBottom: theme.spacing(2),
+    paddingTop: theme.spacing(4),
+    // padding: theme.spacing(4),
+    [theme.breakpoints.down("sm")]: {
+        textAlign: "left",
+
+        paddingBottom: 0,
+    }
+}));
 
 
 const Venue = () => {
@@ -63,19 +113,25 @@ const Venue = () => {
 
     return (
         <VenueContainerStyles>
+            <VenuesTitleStyles variant={"h3"}>Musical Venues of Chattanooga</VenuesTitleStyles>
             <VenueListStyles>
-                <VenueStyles onClick={() => onClick("soldier")} >
-                    <ImgStyles component='img' src={'soldiers_logo_w_bg_md.png'} />
-                </VenueStyles>
-                <VenueStyles  onClick={() => onClick("signal")}>
+
+                <VenueCard onClick={() => onClick("signal")}>
                     <ImgStyles component='img' src={'signal_logo_w_bg_md_white.png'} />
-                </VenueStyles>
-                <VenueStyles  onClick={() => onClick("tivoli")}>
-                    <ImgStyles component='img' src={'tivoli_logo_w_bg_md.png'} />
-                </VenueStyles>
-                <VenueStyles onClick={() => onClick("walker")} >
+                    <TextStyles component='p'>The Signal</TextStyles>
+                </VenueCard>
+                <VenueCard onClick={() => onClick("walker")} >
                     <ImgStyles component='img' src={'walker_logo_no_bg_md.png'} />
-                </VenueStyles>
+                    <TextStyles component='p'>The Walker Theater</TextStyles>
+                </VenueCard>
+                <VenueCard onClick={() => onClick("soldier")}>
+                    <ImgStyles component='img' src={'soldiers_logo_w_bg_md.png'} />
+                    <TextStyles component='p'>Soldiers and Sailors Memorial Auditorium</TextStyles>
+                </VenueCard>
+                <VenueCard  onClick={() => onClick("tivoli")}>
+                    <ImgStyles component='img' src={'tivoli_logo_w_bg_md.png'} />
+                    <TextStyles component='p'>The Tivoli Theater</TextStyles>
+                </VenueCard>
             </VenueListStyles>
         </VenueContainerStyles>
     );
