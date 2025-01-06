00f0c17d35d515eb20556045d5b5e6a91971e5b1
VABIII <plybyvern6@aol.com>
2025-01-05 19:09:42 -0500

    Added venue page styles and navigation logic

--- src/components/HeaderBar.jsx
+++ src/components/HeaderBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Box, styled} from '@mui/material';
-import { NavLink } from 'react-router-dom';
+import {NavLink, useNavigate} from 'react-router-dom';
 import {colors} from "../theme.js";
 
 
@@ -8,89 +8,162 @@ const HeaderStyles = styled(Box)(({theme}) => ({
     display: "flex",
     flexGrow: 1,
     flexDirection: 'row',
-    justifyContent: 'space-around',
+    justifyContent: 'space-between',
     alignItems: "center",
-    height: '5vh',
-    fontSize: '2rem',
+    height: '8vh',
+    fontSize: '1.5rem',
+    backgroundColor: colors.get("nightMoon"),
     color: colors.get("nightMoon"),
+    position: 'sticky',
+    top: 0,
     [theme.breakpoints.down("sm")]: {
-        paddingTop: theme.spacing(4),
+        // paddingTop: theme.spacing(4),
+        height: '7vh',
     }
 }))
 
+const LogoBoxStyles = styled(Box)(({theme}) => ({
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    width: '30%',
+    maxHeight: '7vh',
+    [theme.breakpoints.down("sm")]: {
+        // paddingTop: theme.spacing(4),
+        width: '50%',
+        // marginLeft: theme.spacing(2),
+        maxHeight: '6vh'
+    }
+}));
+
+const LinkBoxStyles = styled(Box)(({theme}) => ({
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '40%',
+    [theme.breakpoints.down("sm")]: {
+        // paddingTop: theme.spacing(4),
+        width: '60%',
+        marginRight: theme.spacing(1),
+
+    }
+}));
+
+const LinkStyles = styled(Box)(({theme}) => ({
+    [theme.breakpoints.down("sm")]: {
+
+    }
+
+}))
+
+// const boxStyles = {
+//     display: 'flex',
+//     flexDirection: 'row',
+//     justifyContent: 'center',
+//     width: '30%',
+//     maxHeight: '7vh',
+//     [theme.breakpoints.down("sm")]: {
+//         // paddingTop: theme.spacing(4),
+//         width: '100%',
+//         marginLeft: '24px',
+//     }
+// }
+//
+// const linkBoxStyles = {
+//     display: 'flex',
+//     flexDirection: 'row',
+//     justifyContent: 'space-around',
+//     width: '40%',
+//     [theme.breakpoints.down("sm")]: {
+//         // paddingTop: theme.spacing(4),
+//     }
+// }
+
 
 export const HeaderBar = () => {
+    const navigate = useNavigate();
+
+    const onClick = () => navigate('/')
+
     return (
         <HeaderStyles>
-            <Box>
-                <NavLink
-                    to={`/`}
-                    textDecoration='none'
-                    className={({ isActive, isPending }) =>
-                        isActive
-                            ? "active"
-                            : isPending
-                                ? "pending"
-                                : ""
-                    }
-                    style={({ isActive, isPending, isTransitioning }) => {
-                        return {
-                            fontWeight: isActive ? "bold" : "",
-                            color: isPending ? "red" : colors.get("nightMoon"),
-                            viewTransitionName: isTransitioning ? "slide" : "",
-                            textDecoration: "none",
-                        };
-                    }}
-                >
-                    Home
-                </NavLink>
-            </Box>
-            <Box>
-                <NavLink
-                    to={`/venues`}
-                    textDecoration='none'
-                    className={({ isActive, isPending }) =>
-                        isActive
-                            ? "active"
-                            : isPending
-                                ? "pending"
-                                : ""
-                    }
-                    style={({ isActive, isPending, isTransitioning }) => {
-                        return {
-                            fontWeight: isActive ? "bold" : "",
-                            color: isPending ? "red" : colors.get("nightMoon"),
-                            viewTransitionName: isTransitioning ? "slide" : "",
-                            textDecoration: "none",
-                        };
-                    }}
-                >
-                    Venues
-                </NavLink>
-            </Box>
-            <Box>
-                <NavLink
-                    to={`/events`}
-                    textDecoration='none'
-                    className={({ isActive, isPending }) =>
-                        isActive
-                            ? "active"
-                            : isPending
-                                ? "pending"
-                                : ""
-                    }
-                    style={({ isActive, isPending, isTransitioning }) => {
-                        return {
-                            fontWeight: isActive ? "bold" : "",
-                            color: isPending ? "red" : colors.get("nightMoon"),
-                            viewTransitionName: isTransitioning ? "slide" : "",
-                            textDecoration: "none",
-                        };
-                    }}
-                >
-                    About
-                </NavLink>
-            </Box>
+            <LogoBoxStyles onClick={onClick}>
+                <Box component="img" src={'clm_logo_xxsm_no_bg.png'}  />
+            </LogoBoxStyles>
+            <LinkBoxStyles >
+                <LinkStyles>
+                    <NavLink
+                        to={`/`}
+                        textDecoration='none'
+                        className={({ isActive, isPending }) =>
+                            isActive
+                                ? "active"
+                                : isPending
+                                    ? "pending"
+                                    : ""
+                        }
+                        style={({ isActive, isPending, isTransitioning }) => {
+                            return {
+                                fontWeight: isActive ? "bold" : "",
+                                color: isPending ? "red" : "white",
+                                // color: isPending ? "red" : colors.get("nightMoon"),
+                                viewTransitionName: isTransitioning ? "slide" : "",
+                                textDecoration: "none",
+                            };
+                        }}
+                    >
+                        Shows
+                    </NavLink>
+                </LinkStyles>
+                <LinkStyles>
+                    <NavLink
+                        to={`/venues`}
+                        textDecoration='none'
+                        className={({ isActive, isPending }) =>
+                            isActive
+                                ? "active"
+                                : isPending
+                                    ? "pending"
+                                    : ""
+                        }
+                        style={({ isActive, isPending, isTransitioning }) => {
+                            return {
+                                fontWeight: isActive ? "bold" : "",
+                                color: isPending ? "red" : "white",
+                                // color: isPending ? "red" : colors.get("nightMoon"),
+                                viewTransitionName: isTransitioning ? "slide" : "",
+                                textDecoration: "none",
+                            };
+                        }}
+                    >
+                        Venues
+                    </NavLink>
+                </LinkStyles>
+                <LinkStyles>
+                    <NavLink
+                        to={`/events`}
+                        textDecoration='none'
+                        className={({ isActive, isPending }) =>
+                            isActive
+                                ? "active"
+                                : isPending
+                                    ? "pending"
+                                    : ""
+                        }
+                        style={({ isActive, isPending, isTransitioning }) => {
+                            return {
+                                fontWeight: isActive ? "bold" : "",
+                                color: isPending ? "red" : "white",
+                                // color: isPending ? "red" : colors.get("nightMoon"),
+                                viewTransitionName: isTransitioning ? "slide" : "",
+                                textDecoration: "none",
+                            };
+                        }}
+                    >
+                        About
+                    </NavLink>
+                </LinkStyles>
+            </LinkBoxStyles>
         </HeaderStyles>
     );
 };
--- src/components/NoShows.jsx
+++ src/components/NoShows.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import {HomeStyles, TitleStyles, ImgStyles, LinkStyles, TextStyles, EventStyles, EventsStyles, LogoStyles, SubtitleStyles, NoShowsBanner, NoShowTitleStyles, NoShowSubTitleStyles,} from "./styledComponents/index.jsx";
+import { NoShowsBanner, NoShowTitleStyles, NoShowSubTitleStyles, } from "./styledComponents/index.jsx";
+import SentimentVeryDissatisfiedIcon from '@mui/icons-material/SentimentVeryDissatisfied';
+import {Box} from "@mui/material";
 
+const iconStyles =  {
+    fontSize: 'inherit',
+    justifySelf: 'center',
+    alignSelf: 'center'
+}
 
 const NoShows = () => {
     return (
         <NoShowsBanner>
-            <NoShowTitleStyles variant={"h3"}>Unfortunately, we do not have any shows for you today. </NoShowTitleStyles>
+            <NoShowTitleStyles variant={"h3"}>Unfortunately, we do not have any shows for you today. <SentimentVeryDissatisfiedIcon sx={iconStyles} /> </NoShowTitleStyles>
+            <Box sx={{fontSize: '128px'}}>
+
+            </Box>
             <NoShowSubTitleStyles variant={"h3"}>Check out these upcoming events or the venue pages for full show lineups.</NoShowSubTitleStyles>
             {/*<TextStyles variant={"h3"}></TextStyles>*/}
 
